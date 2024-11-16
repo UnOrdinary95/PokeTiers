172f14a1590b7774eab2da470f6f3d5ccbcefa72
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2024-11-16 23:25:54 +0100

    Small change

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,6 @@ const bouton_jaune = document.getElementById("yellow_color");
 const bouton_violet = document.getElementById("purple_color");
 const stylesheet = document.getElementById("stylesheet");
 
-// On récupère la sauvegarde si elle existe
-let backup_href = localStorage.getItem("href_backup");
-// backup_href renvoie "null" si elle n'est pas défini donc on va s'en servir pour comparer avec "null" avec '!==' (Comparaison de type & valeur)
-
-// Si backup_href est bien défini alors on va le récupérer afin d'appliquer le bon CSS
-if(backup_href !== null){
-    stylesheet.href = backup_href; 
-}
-
 // Fonction pour changer le style
 function changerStylesheet(anEvent){
     switch(anEvent.target.id){
@@ -29,9 +20,17 @@ function changerStylesheet(anEvent){
         case "purple_color":
             stylesheet.href = "styles/style-purple.css";
     }
-
     // Sauvegarde le choix de style dans le localStorage
     localStorage.setItem("href_backup", stylesheet.href);
+}
+
+// On récupère la sauvegarde si elle existe
+let backup_href = localStorage.getItem("href_backup");
+// backup_href renvoie "null" si elle n'est pas défini donc on va s'en servir pour comparer avec "null" avec '!==' (Comparaison de type & valeur)
+
+// Si backup_href est bien défini alors on va le récupérer afin d'appliquer le bon CSS
+if(backup_href !== null){
+    stylesheet.href = backup_href; 
 }
 
 // Ajoute un écouteur d'évènement au clic
