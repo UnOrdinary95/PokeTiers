7e08f7cba147bfe24853fdd830ffd56526eb39a2
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2025-03-28 11:37:11 +0100

    Update

--- config/init_db.php
+++ config/init_db.php
@@ -3,9 +3,9 @@
 function getDbConnection() {
     // Accéder aux variables d'environnement définies sur AlwaysData en utilisant 'getenv()'
     $dbHost = getenv('DB_HOST');            // Adresse du serveur
-    $dbName = getenv('DB_NAME');            // Nom de la DB
+    $dbName = getenv('DB_NAME_POKETIERS');            // Nom de la DB
     $dbUser = getenv('DB_USERNAME');        // Utilisateur
-    $dbPassword = getenv('DB_PASSWORD');    // Mot de passe
+    $dbPassword = getenv('_DB_PASSWORD');    // Mot de passe
     
     // Construire un string permettant ainsi de se connecter à la DB PostgreSQL
     $dbStr = "host=$dbHost dbname=$dbName user=$dbUser password=$dbPassword";
@@ -19,4 +19,4 @@ function getDbConnection() {
 
     return $connexion;
 }
-?>
+
